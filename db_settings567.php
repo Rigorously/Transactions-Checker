25cf8837042f241ec56054b28a2f8f9157982368
Rigorously <31181988+Rigorously@users.noreply.github.com>
2024-04-15 15:37:28 +0200

    Load database connection config from db.json

--- db_settings567.php
+++ db_settings567.php
@@ -1,13 +1,23 @@
 <?php
 
 //database configuration
-//replace with correct values for your setup
+//create the file db.json outside of the web root
+//{
+//	"db_host": "localhost",
+//	"db_port": 5432,
+//	"db_name": "dbname",
+//	"db_user": "dbuser",
+//	"db_password": "dbpasswd"
+//}
 
-$db_host = 'localhost';
-$db_port = '5432';
-$db_name = 'dbname';  
-$db_user = 'dbuser';
-$db_password = 'dbpasswd';
+$db_file = file_get_contents('../db.json');
+$db_settings = json_decode($db_file);
+
+$db_host = $db_settings->db_host;
+$db_port = $db_settings->db_port;
+$db_name = $db_settings->db_name;
+$db_user = $db_settings->db_user;
+$db_password = $db_settings->db_password;
 
 //end of user settings
 //leave the following untouched
