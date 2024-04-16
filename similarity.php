01bd2a676f983714d82e3390b34dea3c6e7b7143
Rigorously <31181988+Rigorously@users.noreply.github.com>
2024-04-16 22:13:23 +0200

    Decorate external links

--- similarity.php
+++ similarity.php
@@ -15,6 +15,10 @@
 			width: 45%;
 		}
 
+		table.col-15 td:first-child {
+			width: 15%;
+		}
+
 		.txchars {
 			font-size: small;
 			display: block;
@@ -40,6 +44,18 @@
 
 		.levenshtein:hover+.matchPercentage {
 			display: block;
+		}
+
+		a.external::after {
+			content: "";
+			width: 11px;
+			height: 11px;
+			margin-left: 4px;
+			background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' d='M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z'/%3E%3Cpath fill-rule='evenodd' d='M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z'/%3E%3C/svg%3E");
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: contain;
+			display: inline-block;
 		}
 	</style>
 </head>
@@ -176,11 +192,12 @@
 
 		if ($player)
 		{
-			echo "<table><tr><td>Moniker</td><td>" . $player['name'] . "</td></tr>\n";
+			echo "<table class='col-15'><tr><td>Moniker</td><td>" . $player['name'] . "</td></tr>\n";
 			echo "<tr><td>Address</td><td>" . $player['address'] . "</td></tr>\n";
-			echo "<tr><td>Public key</td><td><a href='https://extended-nebb.kintsugi.tech/player/" . $player['public_key'] . "'>" . $player['public_key'] . "</a></td></tr>\n";
+			echo "<tr><td>Public key</td><td><a class='external' href='https://extended-nebb.kintsugi.tech/player/" . $player['public_key'] . "'>" . $player['public_key'] . "</a></td></tr>\n";
 			$scoreboardTime = $playerType == 'Crew' ? "April 16th" : "March 13th";
 			echo "<tr><td>ROIDs</td><td>" . number_format($player['score']) . " as of $scoreboardTime</td></tr>\n";
+			echo "<tr><td>Transfers</td><td><a class='external' href='transactions.php?address=" . $player['address'] . "&mode=transactions'>" . $player['name'] . "</a></td></tr>\n";
 			echo "</table>\n";
 
 			$earlyTransactions = getEarlyTransactions($player['public_key']);
